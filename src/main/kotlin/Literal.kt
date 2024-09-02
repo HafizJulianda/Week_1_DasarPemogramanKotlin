5d75a948f2537d47fef867efeccf4e908a90cfc2
ASUS-PC <hafizjulianda18@gmail.com>
2024-09-02 20:35:48 +0700

    Add Literal.kt

---
+++ src/main/kotlin/Literal.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var intLiteral = 5
+    var doubleliteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolliteral = true
+    println("Int Literal = "+ intLiteral)
+    println("Double Literal = "+ doubleliteral)
+    println("String Literal = "+ stringLiteral)
+    println("charLiteral = " + charLiteral)
+    println("boolLiteral = " + boolliteral)
+}
